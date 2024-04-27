61178b5864d59b9d336d76c1a37b577b25e2f96b
Harsh Yadav <harsh12codes@gmail.com>
2024-04-27 21:48:56 +0530

    added: aceternity ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,13 @@
+import { BackgroundBeams } from "@/components/ui/background-beams";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
+            {/* Demo use of shadcn , acertinity ui */}
             <Button>Kleenestar v2</Button>
+            <BackgroundBeams />
         </main>
     );
 }
