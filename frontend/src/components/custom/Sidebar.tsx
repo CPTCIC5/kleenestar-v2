4b4f29a3ad839aab7208aa60f78d21f8db7fceaa
Harsh Yadav <harsh12codes@gmail.com>
2024-05-02 06:46:46 +0530

    fix: logo

--- frontend/src/components/custom/Sidebar.tsx
+++ frontend/src/components/custom/Sidebar.tsx
@@ -27,7 +27,8 @@ export default function Sidebar() {
                         href="/"
                         className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
                     >
-                        <Icons.logoLight className="h-4 w-4 transition-all group-hover:scale-110" />
+                        <Icons.logoLight className="h-4 w-4 transition-all group-hover:scale-110 dark:hidden" />
+                        <Icons.logoDark className="h-4 w-4 transition-all hidden dark:group-hover:scale-110 dark:block" />
                         <span className="sr-only">Kleenestar</span>
                     </Link>
                     <Separator />
@@ -149,7 +150,8 @@ export default function Sidebar() {
                                     href="/"
                                     className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
                                 >
-                                    <Icons.logoLight className="h-5 w-5 transition-all group-hover:scale-110" />
+                                    <Icons.logoLight className="h-5 w-5 transition-all group-hover:scale-110 dark:hidden" />
+                                    <Icons.logoDark className="h-5 w-5 transition-all hidden dark:group-hover:scale-110 dark:block" />
                                     <span className="sr-only">Kleenestar</span>
                                 </Link>
                                 <Link
