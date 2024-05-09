74ba914d1784a5adec1993af3b8e42b9dc882446
Harsh Yadav <harsh12codes@gmail.com>
2024-05-09 17:34:06 +0530

    fix

--- frontend/src/app/(dashboard)/sidebar/page.tsx
+++ frontend/src/app/(dashboard)/sidebar/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChatSheet } from "@/components/custom/ChatSidebar";
+import { ChatSidebar } from "@/components/custom/ChatSidebar";
 import { SettingsNotificationForm } from "@/components/custom/SettingsNotificationForm";
 import { SettingsProfileForm } from "@/components/custom/SettingsProfileForm";
 import { SettingsSecurityForm } from "@/components/custom/SettingsSecurityForm";
@@ -12,7 +12,7 @@ import { InfoCircledIcon } from "@radix-ui/react-icons";
 export default function Sidebar() {
     return (
         <div className="w-full h-screen flex items-start justify-center flex-1 bg-muted/40 max-sm:pt-[56px]">
-            <ChatSheet />
+            <ChatSidebar />
             <div className=" flex-1 w-full h-full "></div>
         </div>
     );
