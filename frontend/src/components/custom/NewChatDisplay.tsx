b8b977890d3c25d4fdb529b4dfa27bc5525ec957
Harsh Yadav <harsh12codes@gmail.com>
2024-05-15 12:06:12 +0530

    fixes

--- frontend/src/components/custom/NewChatDisplay.tsx
+++ frontend/src/components/custom/NewChatDisplay.tsx
@@ -2,7 +2,7 @@ import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
 
 export function NewChatDisplay() {
     return (
-        <div className="w-full h-full flex flex-col justify-end items-center gap-[30px]">
+        <div className="w-full h-full flex flex-col justify-end items-center gap-[30px] mb-3">
             <span className="font-mainhead font-medium text-[15px]">
                 How may I assist you today?
             </span>
