919ee8e946c2764e61086074d744a42b71651914
Harsh Yadav <harsh12codes@gmail.com>
2024-05-20 03:14:42 +0530

    Fixed : get-started, workspace

--- frontend/src/app/(workspace)/get-started/page.tsx
+++ frontend/src/app/(workspace)/get-started/page.tsx
@@ -1,7 +1,4 @@
-"use client";
-
 import { Icons } from "@/assets/icons";
-import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
@@ -13,20 +10,20 @@ export default function GetStarted() {
                 href="/login"
                 className={cn(
                     buttonVariants({ variant: "secondary" }),
-                    "absolute right-8 top-8 max-sm:right-4 max-sm:top-4 cursor-pointer",
+                    "absolute z-20 right-8 top-8 max-sm:right-4 max-sm:top-4 cursor-pointer",
                 )}
             >
                 Login
             </Link>
-            <div className="relative h-full flex flex-col animate-fade-in-up  w-fit  mx-auto pt-[10%] max-xl:pt-[30%] z-20">
+            <div className="relative h-full flex flex-col animate-fade-in-up  w-fit  mx-auto pt-[10%] max-xl:pt-[30%] z-10">
                 <Icons.logoDark className=" w-[150px] h-[150px] mx-auto max-xl:w-[50px] max-xl:h-[50px]" />
                 <p className="text-5xl  max-xl:text-2xl  text-center font-mainhead font-bold bg-clip-text text-transparent bg-gradient-to-b from-background dark:from-foreground to-neutral-500 dark:to-background-500 py-8">
                     Welcome to Kleenestar
                 </p>
                 <div className=" px-[15%] text-center">
                     <p>
-                        Kleenestar help you get valuable insights from your marketing campaings and
-                        grow your marketing business
+                        Kleenestar collects every event from your marketing effort and provides
+                        real-time decision-making insights and recommendations in natural language
                     </p>
                 </div>
                 <div className="flex justify-center mt-12">
--- frontend/src/app/(workspace)/workspace/page.tsx
+++ frontend/src/app/(workspace)/workspace/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import workspace from "../../../assets/images/workspace.jpg";
@@ -6,55 +5,59 @@ import person from "../../../assets/images/person.jpg";
 import Image from "next/image";
 import Link from "next/link";
 import { Icons } from "@/assets/icons";
-import { useRouter } from "next/navigation";
 
 export default function ChooseStart() {
-    const router = useRouter();
-
     return (
         <div className="relative animate-fade-in-up  max-xl:pt-10 pt-[5%]">
-            <div className="animate-fade-in-up  max-xl:px-10">
+            <div className="animate-fade-in-up  max-xl:px-5">
                 <div>
                     <Icons.logoDark className=" w-[50px] h-[50px] mx-auto max-xl:w-[50px] max-xl:h-[50px]" />
                 </div>
                 <div className="text-[30px] text-center font-bold font-mainhead pt-4">
                     {"Let's get started"}
                 </div>
-                <div className="w-[45%] max-xl:w-[90%] mx-auto pt-4  text-gray-700">
-                    <div className="text-center  w-fit">
+                <div className="w-[45%] max-xl:w-[100%] mx-auto pt-4  text-gray-700">
+                    <div className="text-center w-full max-xl:text-sm">
                         Join a Workspace to collaborate with your team by entering your workspace
-                        code or accepting an invite, or Create a Workspace to set up your own and
-                        begin organizing and analyzing your marketing data across multiple
-                        platforms.
+                        code or Create a Workspace to set up your own and begin organizing and
+                        analyzing your marketing data across multiple platforms.
                     </div>
                 </div>
-                <div className="flex mx-auto gap-[5%] w-fit pt-10 max-xl:flex-col max-xl:gap-4 pb-10">
+                <div className="flex items-center justify-center gap-6 w-full pt-10 max-xl:pt-4 max-xl:flex-col max-xl:gap-4 pb-10">
                     <Link href="/invited-register">
                         <Card
-                            className={cn("h-[310px] w-[300px] hover:shadow-lg hover:scale-[1.02]")}
+                            className={cn(
+                                "h-[300px] w-[300px] max-xl:h-[200px] max-xl:w-[200px] hover:shadow-lg hover:scale-[1.02]",
+                            )}
                         >
-                            <CardContent className={cn("p-0 h-[260px] pt-4 w-[90%] mx-auto")}>
+                            <CardContent className={cn("p-0 h-[90%] py-2 w-[90%] mx-auto")}>
                                 <Image
                                     className="w-full h-full rounded-2xl "
                                     src={workspace}
                                     alt={"workspace"}
                                 ></Image>
+                                <p className="text-center w-full max-xl:text-sm">
+                                    I have a workspace
+                                </p>
                             </CardContent>
-                            <p className="text-center w-full mt-2">I have a workspace</p>
                         </Card>
                     </Link>
                     <Link href="/register">
                         <Card
-                            className={cn("h-[310px] w-[300px] hover:shadow-lg hover:scale-[1.02]")}
+                            className={cn(
+                                "h-[300px] w-[300px] max-xl:h-[200px] max-xl:w-[200px] hover:shadow-lg hover:scale-[1.02]",
+                            )}
                         >
-                            <CardContent className={cn("p-0 h-[260px] pt-4 w-[90%] mx-auto")}>
+                            <CardContent className={cn("p-0 h-[90%] py-2  w-[90%] mx-auto")}>
                                 <Image
                                     className="w-full h-full rounded-2xl "
                                     src={person}
                                     alt={"person"}
                                 ></Image>
+                                <p className="text-center w-full max-xl:text-sm">
+                                    I {"don't"} have a workspace
+                                </p>
                             </CardContent>
-                            <p className="text-center w-full mt-2">I {"don't"} have a workspace</p>
                         </Card>
                     </Link>
                 </div>
