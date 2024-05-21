1e36b6f72ac529a2385c3302c6af9b432c9d4b3e
Harsh Yadav <harsh12codes@gmail.com>
2024-05-21 13:49:34 +0530

    Fixed: Sidebar

--- frontend/src/components/custom/Sidebar.tsx
+++ frontend/src/components/custom/Sidebar.tsx
@@ -1,92 +1,26 @@
 "use client";
 
 import Link from "next/link";
+import { Icons } from "@/assets/icons";
+import { useLogout } from "@/hooks/useLogout";
+
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-
-import {
-    BookText,
-    ClipboardList,
-    DatabaseZap,
-    LogOut,
-    MessageCircleMore,
-    PanelLeft,
-    Speech,
-    UsersRound,
-    Wallet,
-} from "lucide-react";
-import { Button, buttonVariants } from "../ui/button";
-import { Icons } from "@/assets/icons";
+import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
-import { ModeToggle } from "./ModeToggle";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { cn } from "@/lib/utils";
-import React from "react";
-import axios from "axios";
-import Cookies from "js-cookie";
-import { useRouter } from "next/navigation";
-import { toast } from "sonner";
+import { ModeToggle } from "./ModeToggle";
 
 export default function Sidebar() {
-    const navigate = useRouter();
-    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-    const [userDetails, setUserDetails] = React.useState<{
-        id: string;
-        profile: { country: string };
-    }>({
-        id: "",
-        profile: {
-            country: "",
-        },
-    });
-    React.useEffect(() => {
-        const fetchWorkspaceDetails = async () => {
-            try {
-                const response = await axios.get(`/api/workspaces/`, {
-                    withCredentials: true,
-                    headers: {
-                        "ngrok-skip-browser-warning": "69420",
-                        "Content-Type": "application/json",
-                        "X-CSRFToken": Cookies.get("csrftoken"),
-                    },
-                });
-                // console.log(response);
-
-                setIsLoggedIn(true);
-                setUserDetails(response.data[0].root_user);
-            } catch (err) {
-                navigate.push("/");
-            }
-        };
-        fetchWorkspaceDetails();
-    }, [isLoggedIn]);
-
-    const handleLogOut = async () => {
-        try {
-            const response = await axios.post(`/api/auth/logout/`, null, {
-                withCredentials: true,
-                headers: {
-                    "X-CSRFToken": Cookies.get("csrftoken"),
-                },
-            });
-            console.log(response);
-            Cookies.remove("csrftoken");
-            toast.success("Logout successfully !!");
-            setTimeout(() => {
-                navigate.push("/");
-            }, 200);
-        } catch (err) {
-            console.error("Logout Failed");
-            throw new Error("Logout Failed");
-        }
-    };
+    // data of user is needed to display the user's name and profile picture
+    const mutation = useLogout();
 
     return (
         <TooltipProvider>
             <aside className="fixed inset-y-0 left-0 z-40 hidden w-14 flex-col border-r bg-background sm:flex">
                 <nav className="flex flex-col items-center gap-4 px-2 sm:py-4">
                     <Link
-                        href="/"
+                        href="/get-started"
                         className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
                     >
                         <Icons.logoLight className="h-4 w-4 transition-all group-hover:scale-110 dark:hidden" />
@@ -101,7 +35,7 @@ export default function Sidebar() {
                                 href="/chat"
                                 className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                             >
-                                <MessageCircleMore className="h-5 w-5" />
+                                <Icons.solarChatRoundLine className="h-5 w-5" />
                                 <span className="sr-only">Chat</span>
                             </Link>
                         </TooltipTrigger>
@@ -113,7 +47,7 @@ export default function Sidebar() {
                                 href="/blocknotes"
                                 className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                             >
-                                <ClipboardList className="h-5 w-5" />
+                                <Icons.solarClipboardLine className="h-5 w-5" />
                                 <span className="sr-only">Blocknotes</span>
                             </Link>
                         </TooltipTrigger>
@@ -126,7 +60,7 @@ export default function Sidebar() {
                                 href="/knowledge"
                                 className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                             >
-                                <BookText className="h-5 w-5" />
+                                <Icons.solarBook2Line className="h-5 w-5" />
                                 <span className="sr-only">Knowledge</span>
                             </Link>
                         </TooltipTrigger>
@@ -138,7 +72,7 @@ export default function Sidebar() {
                                 href="/channels"
                                 className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                             >
-                                <DatabaseZap className="h-5 w-5" />
+                                <Icons.solarBoltCircleLine className="h-5 w-5" />
                                 <span className="sr-only">Connect channels</span>
                             </Link>
                         </TooltipTrigger>
@@ -151,7 +85,7 @@ export default function Sidebar() {
                                 href="/billing"
                                 className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                             >
-                                <Wallet className="h-5 w-5" />
+                                <Icons.solarAirbudsCaseChargeLine className="h-5 w-5" />
                                 <span className="sr-only">Plans and billing</span>
                             </Link>
                         </TooltipTrigger>
@@ -163,7 +97,7 @@ export default function Sidebar() {
                                 href="/team"
                                 className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                             >
-                                <UsersRound className="h-5 w-5" />
+                                <Icons.solarUserGroupRoundedLine className="h-5 w-5" />
                                 <span className="sr-only">Team members</span>
                             </Link>
                         </TooltipTrigger>
@@ -175,7 +109,7 @@ export default function Sidebar() {
                                 href="/feedback"
                                 className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                             >
-                                <Speech className="h-5 w-5" />
+                                <Icons.solarUserSpeakRoundedLine className="h-5 w-5" />
                                 <span className="sr-only">Support and feedback</span>
                             </Link>
                         </TooltipTrigger>
@@ -183,13 +117,14 @@ export default function Sidebar() {
                     </Tooltip>
                 </nav>
                 <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-4">
+                    <Separator />
                     <Tooltip>
                         <TooltipTrigger asChild>
                             <div
-                                onClick={handleLogOut}
+                                onClick={() => mutation.mutate()}
                                 className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                             >
-                                <LogOut className="h-5 w-5" />
+                                <Icons.solarExitLine className="h-5 w-5" />
                                 <span className="sr-only">Sign out</span>
                             </div>
                         </TooltipTrigger>
@@ -202,7 +137,7 @@ export default function Sidebar() {
                 <Sheet>
                     <SheetTrigger asChild>
                         <Button size="icon" variant="outline" className="sm:hidden">
-                            <PanelLeft className="h-5 w-5" />
+                            <Icons.solarSidebarLine className="h-5 w-5" />
                             <span className="sr-only">Toggle Menu</span>
                         </Button>
                     </SheetTrigger>
@@ -220,56 +155,56 @@ export default function Sidebar() {
                                 href="/chat"
                                 className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                             >
-                                <MessageCircleMore className="h-5 w-5" />
+                                <Icons.solarChatRoundLine className="h-5 w-5" />
                                 Chat
                             </Link>
                             <Link
                                 href="/blocknotes"
                                 className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                             >
-                                <ClipboardList className="h-5 w-5" />
+                                <Icons.solarClipboardLine className="h-5 w-5" />
                                 Blocknotes
                             </Link>
                             <Link
                                 href="/knowledge"
                                 className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                             >
-                                <BookText className="h-5 w-5" />
+                                <Icons.solarBook2Line className="h-5 w-5" />
                                 Knowledge
                             </Link>
                             <Link
                                 href="/channels"
                                 className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                             >
-                                <DatabaseZap className="h-5 w-5" />
+                                <Icons.solarBoltCircleLine className="h-5 w-5" />
                                 Connect channels
                             </Link>
                             <Link
                                 href="/billing"
                                 className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                             >
-                                <Wallet className="h-5 w-5" />
+                                <Icons.solarAirbudsCaseChargeLine className="h-5 w-5" />
                                 Plans and billing
                             </Link>
                             <Link
                                 href="/team"
                                 className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                             >
-                                <UsersRound className="h-5 w-5" />
+                                <Icons.solarUserGroupRoundedLine className="h-5 w-5" />
                                 Team members
                             </Link>
                             <Link
                                 href="/feedback"
                                 className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                             >
-                                <Speech className="h-5 w-5" />
+                                <Icons.solarUserSpeakRoundedLine className="h-5 w-5" />
                                 Feedback
                             </Link>
                             <div
-                                onClick={handleLogOut}
+                                onClick={() => mutation.mutate()}
                                 className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                             >
-                                <LogOut className="h-5 w-5" />
+                                <Icons.solarExitLine className="h-5 w-5" />
                                 Sign out
                             </div>
                         </nav>
@@ -277,25 +212,20 @@ export default function Sidebar() {
                 </Sheet>
                 <div className="flex gap-2 items-center">
                     <ModeToggle />
-
-                    <Link
-                        href={"/settings"}
-                        className={cn(
-                            buttonVariants({ variant: "outline", size: "icon" }),
-                            "rounded-full p-0 z-50",
-                        )}
-                    >
-                        <Avatar className="w-[35px] h-[35px] rounded-full ">
-                            <AvatarImage
-                                className="rounded-full border-2 border-muted"
-                                src="https://github.com/shadcn.png"
-                                alt="@shadcn"
-                            />
-                            <AvatarFallback className="flex items-center justify-center">
-                                N
-                            </AvatarFallback>
-                        </Avatar>
-                        <span className="sr-only">Settings</span>
+                    <Link href="/settings">
+                        <Button variant="outline" size="icon" className="rounded-full p-0 z-50">
+                            <Avatar className="w-[35px] h-[35px] rounded-full ">
+                                <AvatarImage
+                                    className="rounded-full border-2 border-muted"
+                                    src="https://github.com/shadcn.png"
+                                    alt="@shadcn"
+                                />
+                                <AvatarFallback className="flex items-center justify-center">
+                                    N
+                                </AvatarFallback>
+                            </Avatar>
+                            <span className="sr-only">Settings</span>
+                        </Button>
                     </Link>
                 </div>
             </header>
