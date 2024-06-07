9ee93487890c4a55473e670dfe5db3fa4b90b3ee
harsh12codes <harsh12codes@gmail.com>
2024-06-07 12:12:23 +0530

    fix

--- frontend/src/app/(workspace)/workspace/page.tsx
+++ frontend/src/app/(workspace)/workspace/page.tsx
@@ -7,7 +7,8 @@ const ChooseStart: React.FC = () => {
         <div className="relative animate-fade-in-up pt-[5%] max-xl:pt-10">
             <div className="animate-fade-in-up max-xl:px-5">
                 <div>
-                    <Icons.logoDark className="w-[50px] h-[50px] mx-auto" />
+                    <Icons.logoDark className="w-[50px] h-[50px] mx-auto transition-all hover:scale-110 dark:hidden duration-500" />
+                    <Icons.logoLight className="w-[50px] h-[50px] mx-auto transition-all hidden dark:hover:scale-110 dark:block duration-500" />
                 </div>
                 <div className="text-center font-bold font-mainhead text-[30px] pt-4">
                     {"Let's get started"}
@@ -19,7 +20,7 @@ const ChooseStart: React.FC = () => {
                 </div>
                 <div className="flex items-center justify-center gap-6 pt-10 pb-10 max-xl:pt-4 max-xl:flex-col max-xl:gap-4">
                     <Link href="/invited-register">
-                        <Card className="h-[300px] w-[300px] hover:shadow-lg hover:scale-[1.02] max-xl:h-[200px] max-xl:w-[200px] bg-muted">
+                        <Card className="h-[250px] w-[250px] transition-all duration-300 hover:shadow-lg  hover:scale-[1.02] max-xl:h-[200px] max-xl:w-[200px] bg-card">
                             <CardContent className="h-full p-6 flex flex-col items-center justify-between">
                                 <Icons.workspaceYes className="w-[80%] h-[80%]" />
                                 <p className="text-center max-xl:text-sm">I have a workspace</p>
@@ -27,7 +28,7 @@ const ChooseStart: React.FC = () => {
                         </Card>
                     </Link>
                     <Link href="/register">
-                        <Card className="h-[300px] w-[300px] hover:shadow-lg hover:scale-[1.02] max-xl:h-[200px] max-xl:w-[200px] bg-muted">
+                        <Card className="h-[250px] w-[250px] transition-all duration-300 hover:shadow-lg  hover:scale-[1.02] max-xl:h-[200px] max-xl:w-[200px] bg-card">
                             <CardContent className="h-full p-6 flex flex-col items-center justify-between">
                                 <Icons.workspaceNo className="w-[80%] h-[80%] " />
                                 <p className="text-center max-xl:text-sm">
