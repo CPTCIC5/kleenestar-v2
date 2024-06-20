301747348627f89d4aa454a6b21036adf5dab1f5
harsh12codes <harsh12codes@gmail.com>
2024-06-20 21:05:25 +0530

    fixes

--- frontend/src/app/(workspace)/get-started/page.tsx
+++ frontend/src/app/(workspace)/get-started/page.tsx
@@ -1,20 +1,18 @@
-"use client";
-
-import { Icons } from "@/assets/icons";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
-import React, { Suspense } from "react";
-
-const Spline = React.lazy(() => import("@splinetool/react-spline/next"));
+import Spline from "@splinetool/react-spline/next";
+import { Spotlight } from "@/components/ui/spotlight";
 
 const GetStarted: React.FC = () => {
     return (
         <div className="relative h-full flex flex-col items-center animate-fade-in-up pt-[10%] max-xl:pt-[10%] z-10">
+            <Spotlight
+                className="-top-40 left-0 md:left-60 md:-top-20"
+                fill="hsl(var(--foreground))"
+            />
             <div className="h-72 w-full flex justify-center items-center">
-                <Suspense fallback={<Icons.logoDark className="w-52 h-52 blur-md" />}>
-                    <Spline scene="https://prod.spline.design/ecypVgD7JeyQUdGF/scene.splinecode" />
-                </Suspense>
+                <Spline scene="https://prod.spline.design/ecypVgD7JeyQUdGF/scene.splinecode" />
             </div>
 
             <p className="text-2xl sm:text-3xl xl:text-5xl min-[1240px]:text-6xl 2xl:text-7xl   text-center font-mainhead font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-400 dark:from-neutral-400 to-neutral-700 dark:to-neutral-600 ">
